a601d1e66a8fa0a3017e5ed5785a1343c60c81db
Piotr Kasprzyk <kaspiotr@gmail.com>
2021-06-12 17:44:29 +0200

    Add User REST API

---
+++ src/main/java/pl/edu/agh/travelagencyapp/controller/UserController.java
@@ -0,0 +1,74 @@
+package pl.edu.agh.travelagencyapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.edu.agh.travelagencyapp.exception.ResourceNotFoundException;
+import pl.edu.agh.travelagencyapp.model.User;
+import pl.edu.agh.travelagencyapp.repository.UserRepository;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("users")
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long userId)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id " + userId + "not found!"));
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping("users")
+    public User createUser(@RequestBody User user) {
+        return this.userRepository.save(user);
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long userId, @Valid @RequestBody User userDetails)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id " + userId + "not found!"));
+
+        user.setFirstName(userDetails.getFirstName());
+        user.setLastName(userDetails.getLastName());
+        user.setEmail(userDetails.getEmail());
+        user.setRole(userDetails.getRole());
+
+        return ResponseEntity.ok(this.userRepository.save(user));
+    }
+
+    @DeleteMapping("user/{id}")
+    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId) throws ResourceNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id " + userId + "not found!"));
+
+        this.userRepository.delete(user);
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+
+        return response;
+    }
+
+}
--- src/main/java/pl/edu/agh/travelagencyapp/repository/UserRepository.java
+++ src/main/java/pl/edu/agh/travelagencyapp/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package pl.edu.agh.travelagencyapp.repository.exception;
+package pl.edu.agh.travelagencyapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

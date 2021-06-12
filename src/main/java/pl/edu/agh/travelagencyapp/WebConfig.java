061a81fbaca96f19d5cc169be1e29e37054256be
Piotr Kasprzyk <kaspiotr@gmail.com>
2021-06-12 21:38:04 +0200

    Enable CORS

---
+++ src/main/java/pl/edu/agh/travelagencyapp/WebConfig.java
@@ -0,0 +1,23 @@
+package pl.edu.agh.travelagencyapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(MAX_AGE_SECS);
+    }
+
+}

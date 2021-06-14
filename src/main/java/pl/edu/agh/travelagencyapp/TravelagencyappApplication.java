c9524133e143297f75676048eaca51ffdd005267
Piotr Kasprzyk <kaspiotr@gmail.com>
2021-06-14 13:27:39 +0200

    Update Swagger json documentation endpoint and add swagger UI
       * add endpoint with docs <server_address>/v2/api-docs
       * add endpoint with docs <server_address>/v3/api-docs
       * add endpoint with Swagger UI <server_address>>/swagger-ui/index.html?url=/v3/api-docs (http://localhost:8080/swagger-ui/index.html?url=/v3/api-docs)

--- src/main/java/pl/edu/agh/travelagencyapp/TravelagencyappApplication.java
+++ src/main/java/pl/edu/agh/travelagencyapp/TravelagencyappApplication.java
@@ -2,10 +2,8 @@ package pl.edu.agh.travelagencyapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class TravelagencyappApplication {
 
     public static void main(String[] args) {

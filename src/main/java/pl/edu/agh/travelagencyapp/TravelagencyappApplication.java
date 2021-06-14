e96e59e16d409dd5506bcbd964e9a238ce33e957
Piotr Kasprzyk <kaspiotr@gmail.com>
2021-06-14 13:02:57 +0200

    Add Swagger json documentation endpoint
       * add endpoint with docs <server_address>/v2/api-docs

--- src/main/java/pl/edu/agh/travelagencyapp/TravelagencyappApplication.java
+++ src/main/java/pl/edu/agh/travelagencyapp/TravelagencyappApplication.java
@@ -2,8 +2,10 @@ package pl.edu.agh.travelagencyapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TravelagencyappApplication {
 
     public static void main(String[] args) {

db5cb1a478727948d899ba1088a88933effc6f0e
Jakub Nowobilski <nowobilski.jakub@gmail.com>
2021-06-14 09:39:36 +0200

    Add complete model and DB schema

---
+++ src/main/java/pl/edu/agh/travelagencyapp/model/Participant.java
@@ -0,0 +1,66 @@
+package pl.edu.agh.travelagencyapp.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "PARTICIPANTS", schema = "public")
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+
+    public Participant(){}
+
+    public Participant(String firstName, String lastName, LocalDate birthDate, Reservation reservation) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.reservation = reservation;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+}
---
+++ src/main/java/pl/edu/agh/travelagencyapp/model/Trip.java
@@ -0,0 +1,120 @@
+package pl.edu.agh.travelagencyapp.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "TRIPS", schema = "public")
+public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDate endDate;
+
+    @Column(name = "available_places_no", nullable = false)
+    private int availablePlacesNo;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "country", nullable = false)
+    private String country;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "base_price", nullable = false)
+    private int basePrice;
+
+    @OneToMany
+    @JoinColumn(name = "trip_id")
+    private Set<Reservation> reservations;
+
+    public Trip(){}
+
+    public Trip(LocalDate startDate, LocalDate endDate, int availablePlacesNo, String name, String country, String description, int basePrice) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.availablePlacesNo = availablePlacesNo;
+        this.name = name;
+        this.country = country;
+        this.description = description;
+        this.basePrice = basePrice;
+        this.reservations = new HashSet<>();
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getAvailablePlacesNo() {
+        return availablePlacesNo;
+    }
+
+    public void setAvailablePlacesNo(int availablePlacesNo) {
+        this.availablePlacesNo = availablePlacesNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(int basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public Set<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(Set<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    public void addReservation(Reservation reservation){
+        this.reservations.add(reservation);
+    }
+}
